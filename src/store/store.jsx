fdd90ec4440889e8840ca6eb71551a00ac223314
priyanshu-pal88 <aachipal88@gmail.com>
2025-08-17 11:52:57 +0530

    Finally done

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './reducers/userSlice'
-import cartSlice from './reducers/cartSlice'
 import productSlice from './reducers/productSlice'
 export const store = configureStore({
   reducer: {
     userReducer : userSlice,
-    cartReducer : cartSlice,
     productReducer : productSlice
   },
 })
